e9494e664cbe879106d49f60970caf8794b80c36
sefatanam <sefatanam@gmail.com>
2020-12-28 01:06:24 +0600

    single queue

---
+++ rabbitMQ-singleQueue/Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+using RabbitMQ.Client.Events;
+using static System.Console;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:sefat@localhost:5672")
+            };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+            channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+          
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                string messge = Encoding.UTF8.GetString(body);
+                WriteLine(messge);
+            };
+
+            channel.BasicConsume("demo-queue", true, consumer);
+            ReadLine();
+
+        }
+    }
+}
---
+++ rabbitMQ-singleQueue/Producer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:sefat@localhost:5672")
+            };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+            channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            dynamic message = new { Name = "Producer", Message = "Work Done !" };
+            dynamic body = Encoding.UTF32.GetBytes(JsonConvert.SerializeObject(message));
+            channel.BasicPublish("", "demo-queue", true, null, body);
+        }
+    }
+}
