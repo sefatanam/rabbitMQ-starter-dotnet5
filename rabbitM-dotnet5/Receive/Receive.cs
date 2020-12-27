d456a927f5a0f0b94ae6850a6628510fdc5a8e0f
sefatanam <sefatanam@gmail.com>
2020-12-27 14:55:54 +0600

    hello world

---
+++ rabbitM-dotnet5/Receive/Receive.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using static System.Console;
+
+class Receive
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        // var factory = new ConnectionFactory() { HostName = "localhost", Port = 5505, UserName = "rabbit", Password = "12345" };
+
+        using (var connection = factory.CreateConnection())
+        {
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    WriteLine($"[x] Receive {message}");
+                };
+
+                channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+                WriteLine("Press [enter] to exit");
+                ReadLine();
+            }
+        }
+    }
+
+}
---
+++ rabbitM-dotnet5/Send/Send.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using static System.Console;
+
+class Send
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost"};
+        // var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "rabbit", Password = "12345" };
+        // This is how we create Server
+        // The connection abstracts the socket connection, and takes care of protocol version negotiation and 
+        // authentication and so on for us. Here we connect to a RabbitMQ node on the local machine - hence
+        // the localhost. If we wanted to connect to a node on a different machine we'd simply specify its hostname or IP address here.
+        using (var connection = factory.CreateConnection())
+        {
+            using (var channel = connection.CreateModel())
+            {
+                // Next we create a channel, which is where most of the API for getting things done resides.
+
+                channel.QueueDeclare(queue: "Hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                string message = "Hello World";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+                WriteLine($"[x] Sent {message}");
+            }
+
+            WriteLine("Press [enter] to exit");
+            ReadLine();
+        }
+    }
+
+}
